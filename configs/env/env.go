975045ea9d23cee3ff0c0bbc4634e80fde5fd5d7
Inoi-K <greatoat@gmail.com>
2022-10-21 19:31:19 +0300

    simple db pool w/ add command

--- configs/env/env.go
+++ configs/env/env.go
@@ -3,5 +3,6 @@ package env
 import "flag"
 
 var (
-	Token = flag.String("token", "", "Bot token")
+	Token       = flag.String("token", "", "Bot token")
+	DatabaseUrl = flag.String("db-url", "postgres://localhost:5432/test1", "Database URL")
 )
